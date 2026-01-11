adc17aab152861591c3fb5873738dfefe7203451
Darius Liddell <darius@carerev.com>
2026-01-11 13:33:37 +0100

    make sure text and photos do not overlap each other

--- grapes/templatetags/grapes_filters.py
+++ grapes/templatetags/grapes_filters.py
@@ -36,3 +36,76 @@ def title_country(country_name):
       result.append(word.capitalize())
   
   return ' '.join(result)
+
+
+@register.filter
+def break_long_text(text, max_chars=65):
+  """
+  Break text into multiple lines, never exceeding max_chars per line.
+  If a word would be split in the middle, hyphenate it.
+  
+  Example:
+    "university" split at position 4 -> "univ-" on first line, "ersity" on second
+  """
+  if not text or len(text) <= max_chars:
+    return text
+  
+  lines = []
+  current_line = []
+  current_length = 0
+  
+  words = text.split()
+  
+  for word in words:
+    word_len = len(word)
+    
+    # If word fits on current line, add it
+    if current_length == 0:
+      # First word on line
+      if word_len <= max_chars:
+        current_line.append(word)
+        current_length = word_len
+      else:
+        # Word is too long, need to hyphenate
+        while word_len > max_chars:
+          # Take max_chars - 1 (for hyphen) from word
+          part = word[:max_chars - 1]
+          lines.append(part + '-')
+          word = word[max_chars - 1:]
+          word_len = len(word)
+        if word:
+          current_line.append(word)
+          current_length = word_len
+    else:
+      # Check if word fits with a space
+      if current_length + 1 + word_len <= max_chars:
+        current_line.append(word)
+        current_length += 1 + word_len
+      else:
+        # Word doesn't fit, finish current line
+        if current_line:
+          lines.append(' '.join(current_line))
+        current_line = []
+        current_length = 0
+        
+        # Now add the word (might need hyphenation)
+        if word_len <= max_chars:
+          current_line.append(word)
+          current_length = word_len
+        else:
+          # Word is too long, need to hyphenate
+          while word_len > max_chars:
+            # Take max_chars - 1 (for hyphen) from word
+            part = word[:max_chars - 1]
+            lines.append(part + '-')
+            word = word[max_chars - 1:]
+            word_len = len(word)
+          if word:
+            current_line.append(word)
+            current_length = word_len
+  
+  # Add remaining line
+  if current_line:
+    lines.append(' '.join(current_line))
+  
+  return '<br>'.join(lines)
